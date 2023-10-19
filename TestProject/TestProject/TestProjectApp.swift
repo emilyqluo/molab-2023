95b7fd8490068c263a9e28f82068d0c2c75bc9b8
Emily <emily.qian18@gmail.com>
2023-10-19 12:09:11 -0400

    Week 3

---
+++ TestProject/TestProject/TestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestProjectApp.swift
+//  TestProject
+//
+//  Created by Emily Luo on 10/19/23.
+//
+
+import SwiftUI
+
+@main
+struct TestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
