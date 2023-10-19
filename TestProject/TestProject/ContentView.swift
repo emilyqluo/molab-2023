7195a116cc78304a516f5aa897a6802a9cec77d5
Emily <emily.qian18@gmail.com>
2023-10-19 12:15:23 -0400

    Week 3

--- TestProject/TestProject/ContentView.swift
+++ TestProject/TestProject/ContentView.swift
@@ -1,24 +1,34 @@
-//
-//  ContentView.swift
-//  TestProject
-//
-//  Created by Emily Luo on 10/19/23.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color.black.edgesIgnoringSafeArea(.all) // Set the background color
+
+            // Create a ForEach loop to generate multiple random shapes
+            ForEach(0..<10, id: \.self) { _ in
+                RandomShapeView()
+            }
         }
-        .padding()
     }
 }
 
-#Preview {
-    ContentView()
+struct RandomShapeView: View {
+    let shapeColors: [Color] = [.red, .green, .blue, .orange, .purple, .yellow]
+
+    var body: some View {
+        let randomX = CGFloat.random(in: 0..<UIScreen.main.bounds.width)
+        let randomY = CGFloat.random(in: 0..<UIScreen.main.bounds.height)
+        let randomSize = CGFloat.random(in: 50...200)
+        let randomRotation = Angle.degrees(Double.random(in: 0...360))
+        let randomColor = shapeColors.randomElement() ?? .white
+
+        return AnyView(
+            Ellipse()
+                .fill(randomColor)
+                .frame(width: randomSize, height: randomSize)
+                .position(x: randomX, y: randomY)
+                .rotationEffect(randomRotation)
+        )
+    }
 }
